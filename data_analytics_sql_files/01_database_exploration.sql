6e7354f8e4f13415317654cd612e47c1b174ae16
HARSHA-Hub1 <sriharsha143k@gmail.com>
2026-09-25 19:01:04 +0530

    Delete data_analytics_sql_files directory

--- data_analytics_sql_files/01_database_exploration.sql
+++
@@ -1,4 +0,0 @@
--- Database Exploration
-SELECT * FROM gold.dim_customers;
-SELECT * FROM gold.fact_sales t;
-SELECT * FROM gold.dim_products dp;
--- data_analytics_sql_files/03_date_range_exploration.sql
+++
@@ -1,3 +0,0 @@
--- Date Range Exploration
-SELECT MIN(birthdate) AS old_customer, MAX(birthdate) AS latest_customer
-FROM gold.dim_customers dc;
